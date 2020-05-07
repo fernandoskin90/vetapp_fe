68ceb5ca8b0411d36828cb32aa2cb7b36de4d832
David Higuita <david.higuita.75@gmail.com>
2020-05-06 20:33:09 -0500

    Added eslint and husky config

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
 const App = () => (
-    <div>
-        <h1>APP</h1>
-    </div>
-)
+  <div>
+    <h1>APP</h1>
+    {
+        [1, 2, 3].map((i, idx) => (
+          <div key={`item-${idx}`}>{i}</div>
+        ))
+    }
+  </div>
+);
 
-render(<App />, document.querySelector('#app'))
+render(<App />, document.querySelector('#app'));
